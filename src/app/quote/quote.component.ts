4fc6cae4223a8cd5730692403560c14d6c65b64d
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-06-14 11:25:32 +0300

    Add botstrap links to index.html file

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import {Quote  } from "../quote";
 @Component({
   selector: 'app-quote',
@@ -7,15 +7,26 @@ import {Quote  } from "../quote";
 })
 export class QuoteComponent implements OnInit {
 quotes:Quote[]=[
-  {id:1,quote:'"Love the life you live, live the life you love."',author:'Bob Marley'},
-  {id:2,quote:'"Difficulties in your life do not come to destroy you but to help you realise your hidden potential and power. Let difficulties know that you too are difficult."',author:'Avul Pacir Zainulabidin Abdul Kalam'},
-  {id:1,quote:'"Never bend your head. Always hold it high. Look the world straight in the eye."',author:'Helen Keller '},
+  new Quote(1,'"Love the life you live, live the life you love."','Bob Marley'),
+  new Quote(2,'"Difficulties in your life do not come to destroy you but to help you realise your hidden potential and power. Let difficulties know that you too are difficult."','Avul Pacir Zainulabidin Abdul Kalam'),
+  new Quote(1,'"Never bend your head. Always hold it high. Look the world straight in the eye."','Helen Keller '),
 ];
+@Output() toDelete = new EventEmitter<boolean>();
+
+quoteComplete(complete:boolean){
+  this.toDelete.emit(complete);
+}
 deleteQuote(toDelete, index){
    if (toDelete) {
      this.quotes.splice(index,1);
    }
  }
+ upvote(i){
+      this.quotes[i].upvotes+=1
+  }
+downvote(i){
+    this.quotes[i].downvotes+=1
+  }
 
 constructor() { }
 
