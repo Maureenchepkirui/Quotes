795b59af05908953b73c6cde535cfcf1d130483d
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-06-14 16:01:55 +0300

    Fixing bugs to make the thrash icon work

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import {Quote  } from "../quote";
 @Component({
   selector: 'app-quote',
@@ -8,14 +8,9 @@ import {Quote  } from "../quote";
 export class QuoteComponent implements OnInit {
 quotes:Quote[]=[
   new Quote(1,'"Love the life you live, live the life you love."','Bob Marley'),
-  new Quote(2,'"Difficulties in your life do not come to destroy you but to help you realise your hidden potential and power. Let difficulties know that you too are difficult."','Avul Pacir Zainulabidin Abdul Kalam'),
-  new Quote(1,'"Never bend your head. Always hold it high. Look the world straight in the eye."','Helen Keller '),
-];
-@Output() toDelete = new EventEmitter<boolean>();
+  new Quote(2,'"In whatever you do,do it to your best"','Henry Dru'),
 
-quoteComplete(complete:boolean){
-  this.toDelete.emit(complete);
-}
+];
 deleteQuote(toDelete, index){
    if (toDelete) {
      this.quotes.splice(index,1);
