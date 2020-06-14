fdf8fb1725d644069c9268012bddfcab5e92ba3c
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-06-14 22:06:41 +0300

    Import the form module to the app module to enable the application work with any other forms created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { HighlightDirective } from './highlight.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
